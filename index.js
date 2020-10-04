ca5ceabead88c642dc3dddb2709da82d8bcae296
Gavin R. Isgar <gisgar3@gmail.com>
2020-10-04 11:05:11 -0400

    Added new window-creation functions

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const { ipcRenderer } = require('electron');
+let openWindow = (type) => {
+    ipcRenderer.send('open-window', type);
+}
+
 let openNav = () => {
     document.getElementById("sidenav").style.width = "250px";
 }
 let closeNav = () => {
     document.getElementById("sidenav").style.width = "0";
 }
+
+const aboutButton = document.getElementById("aboutButton");
--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 Copyright (c) Gavin R. Isgar 2020
 Developed at Hack Upstate XV (October 3-4, 2020)
 */
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+require("electron-reload") ("./");
 const fs = require("fs");
 
-let createWindow = () => {
+let createMainWindow = () => {
     const win = new BrowserWindow({
         width: 1200,
         height: 850,
         resizable: false,
         maximizable: false,
         fullscreenable: false,
+        backgroundColor: "#343940",
         frame: false,
         title: "Raycord",
+        show: false,
         webPreferences: {
             nodeIntegration: true,
             devTools: true
@@ -21,6 +24,35 @@ let createWindow = () => {
     });
 
     win.loadFile("./index.html");
+
+    win.on('ready-to-show', () => {
+        win.show();
+        win.focus();
+    });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createMainWindow);
+
+ipcMain.on('open-window', (event, arg) => {
+    if (arg == "about") {
+        let win = new BrowserWindow({
+            width: 500,
+            height: 350,
+            show: false,
+            frame: false,
+            resizable: false,
+            maximizable: false,
+            fullscreenable: false,
+            backgroundColor: "#343940",
+            webPreferences: {
+                nodeIntegration: true,
+                devTools: true
+            }
+        });
+        win.loadFile("./routes/about/about.html");
+        win.on('ready-to-show', () => {
+            win.show();
+            win.focus();
+        });
+    }
+});
